81d31a9cebf61cda573d940676fe90231a8f800d
crudust <crudust@users.noreply.github.com>
2026-09-14 01:45:52 +0900

    sync: Update Comics8 Desktop 2026-09-14 01:45:52

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -43,7 +43,7 @@ compose.desktop {
                 "jdk.unsupported",
             )
             packageName = "Comics8"
-            packageVersion = "1.2.17"
+            packageVersion = "1.2.18"
             description = "Comics8 Monitor Desktop"
             macOS {
                 bundleID = "com.comics8.desktop"
